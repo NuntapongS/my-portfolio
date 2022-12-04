256090b73aa4ca2660281cc53f616044b87db711
Nuntapong <nuntapong@odds.team>
2022-12-04 22:51:17 +0700

    add who am i component

--- src/components/whoAmI.tsx
+++ src/components/whoAmI.tsx
@@ -3,7 +3,10 @@ import profile from "../image/S__8454147.jpg";
 const WhoAmI = () => {
   return (
     <div className="body">
-      <div className="head-content">eiei</div>
+      <div className="head-content">
+        eiei
+        <div className="content">sawaddee</div>
+      </div>
       <img src={profile} className="img" alt="" />
     </div>
   );
