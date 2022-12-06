2bfc2bddcf79ce4de83427f3b91d7b637804e624
Nuntapong <nuntapong@odds.team>
2022-12-06 14:42:34 +0700

    add content

--- src/components/whoAmI.tsx
+++ src/components/whoAmI.tsx
@@ -1,11 +1,14 @@
 import profile from "../image/S__8454147.jpg";
+import eiei from "../lang/word.json";
 
 const WhoAmI = () => {
+  const word: { eiei: string } = eiei;
   return (
     <div className="body">
       <div className="head-content">
         Hello
         <h1 className="sub-content">I am Nuntapong Siripanyawong</h1>
+        <h2 className="content">{word.eiei}</h2>
       </div>
 
       <img src={profile} className="img" alt="profile" />
