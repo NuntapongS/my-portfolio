367ec3a15608c1a26a8c6867bb8fff1bc891d537
Nuntapong <nuntapong@odds.team>
2022-12-08 15:34:33 +0700

    add my skill

--- src/components/whoAmI.tsx
+++ src/components/whoAmI.tsx
@@ -1,9 +1,14 @@
 import profile from "../image/S__8454147.jpg";
-import { default as myBelieve, default as myJob } from "../lang/word.json";
+import {
+  default as myBelieve,
+  default as myJob,
+  default as mySkills,
+} from "../lang/word.json";
 
 const WhoAmI = () => {
   const job: { myJob: string } = myJob;
   const believe: { myBelieve: string } = myBelieve;
+  const skill: { mySkills: string } = mySkills;
   return (
     <div className="body">
       <div className="head-content">
@@ -12,9 +17,9 @@ const WhoAmI = () => {
         <h2 className="content">
           {job.myJob}
           {believe.myBelieve}
+          {skill.mySkills}
         </h2>
       </div>
-
       <img src={profile} className="img" alt="profile" />
     </div>
   );
