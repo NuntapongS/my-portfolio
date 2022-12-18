f996e2efd6913c22f9538ec3c6947c6ed2b9c46f
Nuntapong <nuntapong@odds.team>
2022-12-18 21:21:47 +0700

    add backend and frontend skills

--- src/components/mySkills.tsx
+++ src/components/mySkills.tsx
@@ -1,7 +1,14 @@
+import { default as backendSkills } from "../lang/word.json";
+
 const mySkills = () => {
+  const backend: { backendSkills: string } = backendSkills;
   return (
     <div className="body">
       <div className="skill">My Skills</div>
+      <li className="head-language">
+        Backend Skills : {backend.backendSkills}
+      </li>
+      <li className="head-language">Frontend Kills : </li>
     </div>
   );
 };
