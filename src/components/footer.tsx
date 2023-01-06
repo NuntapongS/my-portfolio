74dff839335d70238b755dce515d280850b21407
Nuntapong <nuntapong@odds.team>
2023-01-06 16:30:19 +0700

    refactor: footer to component

---
+++ src/components/footer.tsx
@@ -0,0 +1,48 @@
+import "../styles/footer.css";
+
+const Footer = () => {
+  return (
+    <>
+      <div className="footer">
+        <div className="social">
+          <a
+            href="https://www.facebook.com/bloodzlu5t/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fa-brands fa-facebook fa-2xl"></i>
+          </a>
+          <a href="twitter.com" target="_blank" rel="noreferrer">
+            <i className="fa-brands fa-twitter fa-2xl"></i>
+          </a>
+          <a
+            href="https://github.com/NuntapongS"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fa-brands fa-github fa-2xl"></i>
+          </a>
+          <a
+            href="https://www.instagram.com/nuntapong.sr/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fa-brands fa-instagram fa-2xl"></i>
+          </a>
+          <a
+            href="mailto:nuntapong.sr@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fa-solid fa-envelope fa-2xl"></i>
+          </a>
+          <a href="#linkin" target="_blank" rel="noreferrer">
+            <i className="fa-brands fa-linkedin fa-2xl"></i>
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
